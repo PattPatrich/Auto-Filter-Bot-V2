b6d68f73700eb1c9e57904366694d41c5002ac0d
PattPatrich <82248145+PattPatrich@users.noreply.github.com>
2021-04-22 21:27:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/Patts_Film_Company")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "⭕️ Movie group 🤓 ⭕️", url="https://t.me/Patts_Films")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "SOURCE CODE", url="https://github.com/PattPatrich/Auto-Filter-Bot-V2")
                     ]
                 ]
             ),
